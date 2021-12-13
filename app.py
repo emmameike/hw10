7488e15f958cf547d20d36670680aa083b4c6e5a
Emma Meike <emma-meike@uiowa.edu>
2021-12-12 21:59:16 -0600

    update

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import os
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 
+#12/12/21
+
 AZURE_KEY_VAULT_URL = os.environ["AZURE_KEY_VAULT_URL"]
 
 credential = DefaultAzureCredential()
